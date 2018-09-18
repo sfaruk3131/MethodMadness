3b590e9a54f3304cdc84a07276edb61555c80c5d
sfaruk3131 <sfaruk3131@bths.edu>
2018-09-18 10:14:30 -0400

    End of class update.

--- src/sfaruk/sfarukLib.java
+++ src/sfaruk/sfarukLib.java
@@ -6,4 +6,24 @@ public class sfarukLib {
     {
         System.out.println(str);
     }
+
+    public static boolean is Palendrome(String word)
+    {
+        String Back = "";
+        boolean Same = true;
+        if(word.length() == 1)
+        {
+            return true;
+        }
+
+        for(int x = word.length() - 1 ; x >= 0 ; x--)
+        {
+            Back = Back + word.substring(x,x + 1);
+        }
+
+        while(Same == true)
+        {
+            
+        }
+    }
 }

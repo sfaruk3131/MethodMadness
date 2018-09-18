2b4d65d43fad90cd825c68bb8c1c5742fad33cc7
BT_1N3_25@bktech.local <BT_1N3_25@bktech.local>
2018-09-18 09:17:31 -0400

    Initial commit

---
+++ src/sfaruk/sfarukLib.java
@@ -0,0 +1,9 @@
+package sfaruk;
+
+public class sfarukLib {
+
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/sfaruk/testfile.java
@@ -0,0 +1,11 @@
+package sfaruk;
+
+import static sfaruk.sfarukLib.println;
+
+public class testfile {
+
+    public static void main(String[] args)
+    {
+        println("I love a cup of Java in the morning.");
+    }
+}
